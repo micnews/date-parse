e996907e7020d41c6831e324403f7aaddecbbc19
Tejesh Mehta <tejesh.mehta@gmail.com>
2016-08-08 06:33:52 -0400

    fix null and boolean cases

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function(val) {
+  if (val === null || typeof val === 'boolean') {
+    return null
+  }
   var d = new Date(val);
   return isNaN(d.getTime()) ? null : d;
 };
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var dateParse = require('./');
 
 test('dateParse', function(t) {
-  t.plan(6);
+  t.plan(8);
   var d1 = dateParse('2014-12-30T21:17:46.826Z');
   t.ok(d1 instanceof Date);
   var d2 = dateParse('not a date');
@@ -15,4 +15,8 @@ test('dateParse', function(t) {
   t.ok(null === d5);
   var d6 = dateParse(null);
   t.ok(null === d6);
+  var d7 = dateParse(false);
+  t.ok(null === d7);
+  var d8 = dateParse(true);
+  t.ok(null === d8);
 });
