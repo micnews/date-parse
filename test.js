c3fcbb28424002aa4ba4ee45776e6753ed91299e
Tejesh Mehta <tejesh.mehta@gmail.com>
2016-08-08 01:40:14 -0300

    Added undefined test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var dateParse = require('./');
 
 test('dateParse', function(t) {
-  t.plan(4);
+  t.plan(5);
   var d1 = dateParse('2014-12-30T21:17:46.826Z');
   t.ok(d1 instanceof Date);
   var d2 = dateParse('not a date');
@@ -11,4 +11,6 @@ test('dateParse', function(t) {
   t.ok(d3 instanceof Date);
   var d4 = dateParse(new Date());
   t.ok(d4 instanceof Date);
+  var d5 = dateParse();
+  t.ok(null === d5);
 });
